b61182b23a2a89018c7c9312d2f7a9ba1f47e05f
comrade-del <41899384+comrade-del@users.noreply.github.com>
2022-07-26 14:07:18 +0100

    Add files via upload

---
+++ battery/spindler.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from abc import ABC
+
+from car import Car
+
+
+class SpindlerBattery(Car, ABC):
+    def __init__(self, last_service_date, current_date):
+        super().__init__(last_service_date)
+        self.current_date = current_date.today().date()
+
+    def test_battery_should_be_serviced(self):
+        return self.current_date - self.last_service_date > 2
